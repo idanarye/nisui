d49c78d2da2f25ac2ac0e0e35d6bca136184d7b1
Travis CI User <travis@example.org>
2020-02-14 13:16:45 +0000

    Update documentation

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"nisui.cli"},{"l":"nisui.cli.print_formats"},{"l":"nisui.core"},{"l":"nisui.core.plotting"},{"l":"nisui.core.util"},{"l":"nisui.h2_store"},{"l":"nisui.java_runner"},{"l":"nisui.simple_reactor"}]
